74ce8bfbd74b69bece94edbf260402176c6e75f6
Detark333 <Detark333@yandex.ru>
2020-01-24 17:42:02 +0400

    qwe

---
+++ DefaultClass.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsTest
+{
+    class DefaultClass
+    {
+        private int[] array;
+        public string ChetSum(int N)
+        {
+            string str = "";
+            int sum = 0;
+            if (array == null)
+            {
+                array = new int[N];
+                Random rand = new Random();
+                for (int i = 0; i < N; ++i)
+                {
+                    array[i] = rand.Next(1, 10);
+                }
+                for (int i = 0; i < N; ++i)
+                {
+                    if ((array[i] > 0) && (array[i] % 2 == 0))
+                    {
+                        sum++;
+                    }
+                }
+                str += sum;
+                return str;
+            } else
+            {
+                for (int j = 0; j < N; ++j)
+                {
+                    if ((array[j] > 0) && (array[j] % 2 == 0))
+                    {
+                        sum++;
+                    }
+                }
+                str += sum;
+                return str;
+            }
+            
+        }
+
+        public string NeChetSum(int N)
+        {
+            string str = "";
+            int sum = 0;
+            if (array == null)
+            {
+                array = new int[N];
+                Random rand = new Random();
+                for (int i = 0; i < N; ++i)
+                {
+                    array[i] = rand.Next(1, 10);
+                }
+                for (int i = 0; i < N; ++i)
+                {
+                    if ((array[i] > N) && (array[i] % 2 != 0))
+                    {
+                        sum++;
+                    }
+                }
+            } else
+            {
+                for (int j = 0; j < N; ++j)
+                {
+                    if ((array[j] > N) && (array[j] % 2 != 0))
+                    {
+                        sum++;
+                    }
+                }
+            }
+            str += sum;
+            return str;
+        }
+
+        public string PrintArray(int N)
+        {
+            string str = "";
+            for(int i = 0; i < N; ++i)
+            {
+                str += array[i];
+            }
+            return str;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DefaultClass cl1;
+        private void Createbutton_Click(object sender, EventArgs e)
+        {
+            cl1 = new DefaultClass();
+            
+        }
+
+        private void ChetSumButton_Click(object sender, EventArgs e)
+        {
+            int size = Convert.ToInt32(ReadtextBox.Text);
+            OutputtextBox.Text = cl1.ChetSum(size);
+            ArraytextBox.Text = cl1.PrintArray(size);
+
+        }
+
+        private void NeChetbutton_Click(object sender, EventArgs e)
+        {
+            int size = Convert.ToInt32(ReadtextBox.Text);
+            OutputtextBox.Text = cl1.NeChetSum(size);
+            ArraytextBox.Text = cl1.PrintArray(size);
+
+        }
+    }
+}
